afe0cc2bb2a463e0750479c49f561c542391b13e
Rafael Couto <rafaelsimoes133@gmail.com>
2022-04-05 17:11:17 -0300

    first commit

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    try {
+      const user = this.listAllUsersUseCase.execute({ user_id } as {
+        user_id: string;
+      });
+      return response.status(201).json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,17 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userAdmin = this.usersRepository.findById(user_id);
+
+    if (!userAdmin) {
+      throw new Error("User does not exists");
+    }
+
+    if (userAdmin.admin === false) {
+      throw new Error("User is not administrator");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
